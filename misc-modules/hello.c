64d99a800859899bd343e7526dd2821b481480d7
lizsPC <lizzyhllrn@gmail.com>
2023-11-06 09:29:25 -0700

    added username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -5,15 +5,16 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world from lizzyhllrn\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world from lizzyhllrn\n");
 }
 
 module_init(hello_init);
